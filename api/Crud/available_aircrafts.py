a9616f9c1642217a05c1fa3ec34513253e47bc8b
Victor Borba <victorvb99@gmail.com>
2022-10-17 19:38:35 -0300

    Correções

--- api/Crud/available_aircrafts.py
+++ api/Crud/available_aircrafts.py
@@ -31,7 +31,7 @@ def getAvailableAircrafts(company_id):
         where = f"where available = 1 and company_id = {company_id}"
     df = pd.read_sql(f"select a.*, c.name as company_name from available_aircraft a join company c on a.company_id = c.id {where}", dbConnection)
     dbConnection.close()
-    if len(df) > 0:
+    if len(df) > 0 and where != "":
         df["files"] = df.apply(lambda x: os.listdir(mypath + x["photos_path"]), axis=1)
     return df.to_dict("records")
 
