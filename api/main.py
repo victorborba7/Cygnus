43c5f12ec9d2e7e3d89efcd0192767cc67c2579a
Victor Borba <victorvb99@gmail.com>
2022-12-12 08:24:27 -0300

    teste requests

--- api/main.py
+++ api/main.py
@@ -24,7 +24,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.post("/user/add")
+@app.post("/api/user/add")
 async def addUser(request: str = Form(...)):
     req = json.loads(request)
     try:
@@ -35,7 +35,7 @@ async def addUser(request: str = Form(...)):
     except:
         raise HTTPException(status_code=400, detail="Erro ao cadastrar usuário")
 
-@app.post("/user/update")
+@app.post("/api/user/update")
 async def changeUser(request: str = Form(...)):
     req = json.loads(request)
     try:
@@ -43,37 +43,37 @@ async def changeUser(request: str = Form(...)):
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
-@app.get("/user/list")
+@app.get("/api/user/list")
 def readUsers():
     return getUsers()
 
-@app.get("/user/get/id")
+@app.get("/api/user/get/id")
 def readUserById(id):
     dbUser = getUserById(id)
     if dbUser is None:
         raise HTTPException(status_code=404, detail="Usuário não encontrado")
     return dbUser
 
-@app.get("/user/get/email")
+@app.get("/api/user/get/email")
 def readUserByEmail(email):
     dbUser = getUserByEmail(email)
     if dbUser is None:
         raise HTTPException(status_code=404, detail="Usuário não encontrado")
     return dbUser
 
-@app.get("/user/get/username")
+@app.get("/api/user/get/username")
 def readUserByUsername(username):
     dbUser = getUserByUsername(username)
     if dbUser is None:
         raise HTTPException(status_code=404, detail="Usuário não encontrado")
     return dbUser
 
-@app.post("/user/delete")
+@app.post("/api/user/delete")
 async def removeUser(request: str = Form(...)):
     req = json.loads(request)
     return {"success": deleteUser(req["id"])}
 
-@app.post("/authenticate")
+@app.post("/api/authenticate")
 async def authenticateUser(request: Request):
     user = await request.json()
     dbUser = getUserByUsername(user["username"])
@@ -87,97 +87,97 @@ async def authenticateUser(request: Request):
             return {"success": True, "user": dbUser}
 
 
-@app.post("/company/add")
+@app.post("/api/company/add")
 async def addCompany(request: str = Form(...), file: UploadFile = File(...)):
     req = json.loads(request)
     req["photo_path"] = await saveLogo(req, file)
     return {"success": createCompany(req)}
 
-@app.post("/company/update")
+@app.post("/api/company/update")
 async def changeCompany(request: str = Form(...), file: UploadFile = File(...)):
     req = json.loads(request)
     await deleteLogo(req)
     req["photo_path"] = await saveLogo(req, file)
     return {"success": updateCompany(req)}
 
-@app.get("/company/list")
+@app.get("/api/company/list")
 def readCompanies():
     companies = getCompanies()
     return companies
 
-@app.get("/company/get/id")
+@app.get("/api/company/get/id")
 def readCompanyById(id):
     dbCompany = getCompanyById(id)
     if dbCompany is None:
         raise HTTPException(status_code=404, detail="Marca não encontrada")
     return dbCompany
 
-@app.get("/company/get/name")
+@app.get("/api/company/get/name")
 def readCompanyByName(name):
     dbCompany = getCompanyByName(name)
     if dbCompany is None:
         raise HTTPException(status_code=404, detail="Marca não encontrada")
     return dbCompany
 
-@app.post("/company/delete")
+@app.post("/api/company/delete")
 async def removeCompany(request: str = Form(...)):
     req = json.loads(request)
     return {"success": deleteCompany(req["id"])}
 
 
-@app.post("/aircraft/add")
+@app.post("/api/aircraft/add")
 async def addAircraft(request: str = Form(...), internos: List[UploadFile] = File(...), externos: List[UploadFile] = File(...), mapa_assentos: Optional[UploadFile] = File(...)):
     req = json.loads(request)
     return {"success": await createAircraft(req, internos, externos, mapa_assentos)}
 
-@app.post("/aircraft/update")
+@app.post("/api/aircraft/update")
 async def changeAircraft(request: str = Form(...), internos: List[UploadFile] = File(...), externos: List[UploadFile] = File(...), mapa_assentos: Optional[UploadFile] = File(...)):
     req = json.loads(request)
     req["photos_path"] = await saveImages(req, internos, externos, mapa_assentos)
     return {"success": updateAircraft(req)}
 
-@app.get("/aircraft/list")
+@app.get("/api/aircraft/list")
 def readAircrafts(company_id):
     aircrafts = getAircrafts(company_id)
     return aircrafts
 
-@app.get("/aircraft/get")
+@app.get("/api/aircraft/get")
 def readAircraftById(id):
     db_aircraft = getAircraftById(id)
     if db_aircraft is None:
         raise HTTPException(status_code=404, detail="aircraft not found")
     return db_aircraft
 
-@app.post("/aircraft/delete")
+@app.post("/api/aircraft/delete")
 async def removeAircraft(request: str = Form(...)):
     req = json.loads(request)
     return {"success": deleteAircraft(req["id"])}
 
 
-@app.post("/available/aircraft/add")
+@app.post("/api/available/aircraft/add")
 async def addAvailableAircraft(request: str = Form(...), photos: List[UploadFile] = File(...)):
     req = json.loads(request)
     return {"success": await createAvailableAircraft(req, photos)}
 
-@app.post("/available/aircraft/update")
+@app.post("/api/available/aircraft/update")
 async def changeAvailableAircraft(request: str = Form(...), photos: List[UploadFile] = File(...)):
     req = json.loads(request)
     req["photos_path"] = await saveImagesA(req, photos)
     return {"success": updateAvailableAircraft(req)}
 
-@app.get("/available/aircraft/list")
+@app.get("/api/available/aircraft/list")
 def readAvailableAircrafts(company_id):
     aircrafts = getAvailableAircrafts(company_id)
     return aircrafts
 
-@app.get("/available/aircraft/get")
+@app.get("/api/available/aircraft/get")
 def readAvailableAircraftById(id):
     db_aircraft = getAvailableAircraftById(id)
     if db_aircraft is None:
         raise HTTPException(status_code=404, detail="aircraft not found")
     return db_aircraft
 
-@app.post("/available/aircraft/delete")
+@app.post("/api/available/aircraft/delete")
 async def removeAvailableAircraft(request: str = Form(...)):
     req = json.loads(request)
     return {"success": deleteAvailableAircraft(req["id"])}
